f244809f482a92e12b9339ee999ef73269dd0f8f
zengzifeng <2511662615@qq.com>
2020-06-28 08:44:55 +0800

    5

---
+++ jenkins-test/src/main/java/com/example/jenkins/jenkinstest/controller/TestJenkinsController.java
@@ -0,0 +1,17 @@
+package com.example.jenkins.jenkinstest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zzf
+ */
+@RestController
+@RequestMapping("jenkins")
+public class TestJenkinsController {
+    @GetMapping("hello")
+    public String hello(){
+        return "hello jenkins";
+    }
+}
